f978684192587212124789f8deb2b53034e299e7
Damien Thouvenin <damien.thouvenin@goood.pro>
2016-07-14 21:07:02 +0200

    essai de maj partielle lors du déploiement

--- deploy-beta.sh
+++ deploy-beta.sh
@@ -1,22 +1,21 @@
 echo "Entrez le descriptif des changements et tapez [ENTER]: "
 read commitmsg
 set -e
-# GITURL=https://github.com/gooodhub/goood-site-dev.git
-cd dist
-echo "git pull"
-git pull origin master:gh-pages --force
-echo "rm "
-ls -a | grep -v '.*' | xargs rm -rf
-cd ..
+GITURL=https://github.com/gooodhub/goood-site-dev.git
 echo "npm install"
 npm install
 NODE_ENV=production node index.js
 cd dist
-# rm -rf .git/
-# git init
-# git remote add origin $GITURL
+rm -rf .git/
+git init
+git remote add origin $GITURL
+echo "git pull"
+git pull origin master:gh-pages
+echo "rm "
+ls -a | grep -v '.*' | xargs rm -rf
 echo "git add"
 git add .
 git commit -am "$commitmsg"
 echo "git push"
 git push origin master:gh-pages --force
+cd ..
