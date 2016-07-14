2c5c1d8c04c2d081c42a1794131ecd5d52d1b090
Damien Thouvenin <damien.thouvenin@goood.pro>
2016-07-14 21:01:29 +0200

    Test

--- deploy-beta.sh
+++ deploy-beta.sh
@@ -1,13 +1,22 @@
 echo "Entrez le descriptif des changements et tapez [ENTER]: "
 read commitmsg
 set -e
-GITURL=https://github.com/gooodhub/goood-site-dev.git
+# GITURL=https://github.com/gooodhub/goood-site-dev.git
+cd dist
+echo "git pull"
+git pull origin master:gh-pages --force
+echo "rm "
+ls -a | grep -v '.*' | xargs rm -rf
+cd ..
+echo "npm install"
 npm install
 NODE_ENV=production node index.js
 cd dist
-rm -rf .git/
-git init
-git remote add origin $GITURL
+# rm -rf .git/
+# git init
+# git remote add origin $GITURL
+echo "git add"
 git add .
 git commit -am "$commitmsg"
+echo "git push"
 git push origin master:gh-pages --force
--- src/index.md
+++ src/index.md
@@ -6,9 +6,6 @@ permalink: false
 ---
 
 # Hello Goood.
-## Titre 2
-### Titre 3
 
-Texte de paragraphe
 
 [A propos](/a-propos-de-goood.html)
