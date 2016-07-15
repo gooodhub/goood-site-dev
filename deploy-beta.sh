9d1d9c282e8c5dacc045ecc60c4229824d7cec38
Damien Thouvenin <damien.thouvenin@goood.pro>
2016-07-15 10:33:26 +0200

    Modif du script de déploiement en beta pour conserver l'historique des changements

--- deploy-beta.sh
+++ deploy-beta.sh
@@ -1,5 +1,10 @@
-echo "Entrez le descriptif des changements et tapez [ENTER]: "
-read commitmsg
+echo "########## Déploiement en béta ##########" 
+read "Entrez le descriptif des changements et tapez [ENTRÉE]: " commitmsg
+if test commitmsg -eq ""
+then
+    echo "Commit message empty. Exiting."
+    exit -1
+fi
 set -e
 GITURL=https://github.com/gooodhub/goood-site-dev.git
 if [ -d "./dist" ]
@@ -9,7 +14,7 @@ fi
 git clone -b gh-pages --single-branch $GITURL dist
 cd dist
 git checkout gh-pages
-ls -a | grep -v '^\.$' | grep -v '^\.\.$' | grep -v '^\.git$' | xargs rm -rf
+# ls -a | grep -v '^\.$' | grep -v '^\.\.$' | grep -v '^\.git$' | xargs rm -rf
 mv .git ../gitdeploy
 cd ..
 npm install
