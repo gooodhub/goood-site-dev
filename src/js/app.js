c8e9327761cf8e102cb32a4fe0bf5ca9a8aad707
Stéphane Montlouis-Calixte <smontlouis@kilix.fr>
2016-07-26 13:31:08 +0200

    Switched to page.js, started home integ

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 import 'whatwg-fetch';
-import { createHistory } from 'history';
+import page from 'page';
 
 import createCarousel from './createCarousel';
 import renderSlides from './renderSlides';
-import handleScrollBehaviour from './scrollBehaviour';
 import pagesData from './pages';
-import { toDomElement, logToHtml } from './helpers';
+import { toDomElement } from './helpers';
+// import handleScrollBehaviour from './scrollBehaviour';
 
 const goood = () => {
   let currentPage = null;
@@ -17,9 +17,21 @@ const goood = () => {
   const cache = {};
   const firstElement = document.querySelector('.slide') || document.querySelector('.subPage');
   const carouselWrapper = document.querySelector('.wrap');
-  const history = createHistory();
 
-  history.listen(changePage); // Trigger changePage() on every PushState
+  // Init routing
+  page('/:slug?', changePage);
+  page('/:slug/:subSlug', changeSubPage);
+  page({
+    popstate: false,
+  });
+
+  window.onpopstate = (e) => {
+    const path = e.state.path;
+    e.state.popstate = true;
+    page.replace(path, e.state);
+  };
+
+
   cache[document.location.pathname] = document.documentElement.innerHTML; // Save current page to cache
   currentPage =
     pagesData.find((p) => p.id === firstElement.id) ||
@@ -45,7 +57,7 @@ const goood = () => {
     bindEvents(currentPage);
 
     // @TODO - Delete for prod
-    logToHtml('#log');
+    // logToHtml('#log');
     document.getElementById('next').addEventListener('click', carousel.nextPane);
     document.getElementById('prev').addEventListener('click', carousel.prevPane);
   }
@@ -62,8 +74,8 @@ const goood = () => {
 
     const path = currentPage.path;
 
-    if (history.getCurrentLocation().pathname !== path) {
-      history.push(path);
+    if (document.location.pathname !== path) {
+      page(path);
     }
   }
 
@@ -87,7 +99,7 @@ const goood = () => {
       previousPage.getDOMContent().innerHTML = '';
       previousPage.onLeaveCompleted();
     }
-    handleScrollBehaviour(currentPage);
+    // handleScrollBehaviour(currentPage);
     currentPage.onEnterCompleted();
   }
 
@@ -97,40 +109,41 @@ const goood = () => {
   * @param {Number} index
   * @param {Number} prevIndex
   */
-  function changePage(location) {
+  function changePage(ctx) {
     if (isSubPageOpened) {
       document.querySelector('.subPageContainer').innerHTML = '';
       isSubPageOpened = false;
     }
 
-    // If subpage
-    if (location.pathname.split('/')[2]) {
-      loadPage(location.pathname)
-      .then(body => {
-        const bodyEl = toDomElement(body);
-        const content = bodyEl.querySelector('.subPageContainer').innerHTML;
+    const pageItem = pagesData.find((p) => p.path === ctx.pathname);
 
-        document.title = bodyEl.title;
-        document.querySelector('.subPageContainer').innerHTML = content;
+    if (ctx.state.popstate && !isSubPageOpened) carousel.showPane(pageItem.position);
+    canBindEvents = true;
 
-        isSubPageOpened = true;
-      });
-    } else {
-      const page = pagesData.find((p) => p.path === location.pathname);
+    loadPage(ctx.pathname)
+    .then(body => {
+      const bodyEl = toDomElement(body);
+      const content = bodyEl.querySelector('.slide__content').innerHTML;
 
-      if (location.action === 'POP' && !isSubPageOpened) carousel.showPane(page.position);
-      canBindEvents = true;
-
-      loadPage(location.pathname)
-      .then(body => {
-        const bodyEl = toDomElement(body);
-        const content = bodyEl.querySelector('.slide__content').innerHTML;
-
-        document.title = bodyEl.title;
-        page.getDOMContent().innerHTML = content;
-      });
-    }
+      document.title = bodyEl.title;
+      pageItem.getDOMContent().innerHTML = content;
+    });
   }
+
+  function changeSubPage(ctx) {
+    // If subpage
+    loadPage(ctx.pathname)
+    .then(body => {
+      const bodyEl = toDomElement(body);
+      const content = bodyEl.querySelector('.subPageContainer').innerHTML;
+
+      document.title = bodyEl.title;
+      document.querySelector('.subPageContainer').innerHTML = content;
+
+      isSubPageOpened = true;
+    });
+  }
+
 
   /**
   * Fetch page according to path - If page in cache, return cache instead
