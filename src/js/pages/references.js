ec8b62b4549b3a72b2bcf9581a30a9f6621df174
Stéphane Montlouis-Calixte <smontlouis@kilix.fr>
2016-08-05 16:56:41 +0200

    80% references

--- src/js/pages/references.js
+++ src/js/pages/references.js
@@ -1,3 +1,5 @@
+import Flickity from 'flickity';
+
 export default {
   position: 3,
   id: 'references',
@@ -8,7 +10,20 @@ export default {
 };
 
 function onEnterCompleted() {
+  // Demarche section
 
+  const carouselOptions = {
+    accessibility: true,
+    resize: true,
+    pageDots: true,
+    prevNextButtons: false,
+    dragThreshold: 30,
+    watchCSS: true,
+  };
+
+  const refCarousel1 = new Flickity('.slide__carousel--operations', carouselOptions);
+  const refCarousel2 = new Flickity('.slide__carousel--organisation', carouselOptions);
+  const refCarousel3 = new Flickity('.slide__carousel--offre', carouselOptions);
 }
 
 function onLeaveCompleted() {
