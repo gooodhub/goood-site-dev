782225affa4c0fa471e37d970896670194fed5d4
Stéphane Montlouis-Calixte <smontlouis@kilix.fr>
2016-08-05 07:46:06 +0200

    Page recette bouclée

--- js/pages/recette.js
+++ js/pages/recette.js
@@ -17,40 +17,16 @@ export default {
   },
 };
 
+function onLeaveCompleted() {
+  this.scrollMagic.controller.destroy(true);
+  this.scrollMagic.controller = null;
+  this.scrollMagic.scenes = [];
+}
+
 function onEnterCompleted() {
+  // Fixed section
   this.scrollMagic.controller = new ScrollMagic.Controller({
     container: '#recette-magique',
-  });
-
-  const demarcheCarousel = new Flickity('.demarche__carousel', {
-    accessibility: true,
-    resize: true,
-    pageDots: false,
-    prevNextButtons: false,
-    dragThreshold: 30,
-  });
-
-  const onCarouselSelect = () => {
-    [...document.querySelectorAll('.demarche-circle')].forEach(item => item.classList.remove('active'));
-    [...document.querySelectorAll('.demarche-circle')][demarcheCarousel.selectedIndex].classList.add('active');
-
-    [...document.querySelectorAll('.demarche__label')].forEach(item => item.classList.remove('demarche__label--active'));
-    [...document.querySelectorAll('.demarche__label')][demarcheCarousel.selectedIndex].classList.add('demarche__label--active');
-  };
-
-  demarcheCarousel.on('select', onCarouselSelect);
-
-  [...document.querySelectorAll('.demarche__item__prev')].forEach(item => item.addEventListener('click', () => demarcheCarousel.previous(true)));
-  [...document.querySelectorAll('.demarche__item__next')].forEach(item => item.addEventListener('click', () => demarcheCarousel.next(true)));
-
-  [...document.querySelectorAll('.demarche-circle')].forEach((item, i) => {
-    if (i === 0) item.classList.add('active');
-    item.addEventListener('click', () => demarcheCarousel.select(i));
-  });
-
-  [...document.querySelectorAll('.demarche__label')].forEach((item, i) => {
-    if (i === 0) item.classList.add('demarche__label--active');
-    item.addEventListener('click', () => demarcheCarousel.select(i));
   });
 
 
@@ -104,8 +80,54 @@ function onEnterCompleted() {
     .addIndicators()
     .addTo(this.scrollMagic.controller)
   );
-}
 
-function onLeaveCompleted() {
 
+  // Demarche section
+  const demarcheCarousel = new Flickity('.demarche__carousel', {
+    accessibility: true,
+    resize: true,
+    pageDots: false,
+    prevNextButtons: false,
+    dragThreshold: 30,
+  });
+
+  const onCarouselSelect = () => {
+    [...document.querySelectorAll('.demarche-circle')].forEach(item => item.classList.remove('active'));
+    [...document.querySelectorAll('.demarche-circle')][demarcheCarousel.selectedIndex].classList.add('active');
+
+    [...document.querySelectorAll('.demarche__label')].forEach(item => item.classList.remove('demarche__label--active'));
+    [...document.querySelectorAll('.demarche__label')][demarcheCarousel.selectedIndex].classList.add('demarche__label--active');
+  };
+
+  demarcheCarousel.on('select', onCarouselSelect);
+
+  [...document.querySelectorAll('.demarche__item__prev')].forEach(item => item.addEventListener('click', () => demarcheCarousel.previous(true)));
+  [...document.querySelectorAll('.demarche__item__next')].forEach(item => item.addEventListener('click', () => demarcheCarousel.next(true)));
+
+  [...document.querySelectorAll('.demarche-circle')].forEach((item, i) => {
+    if (i === 0) item.classList.add('active');
+    item.addEventListener('click', () => demarcheCarousel.select(i));
+  });
+
+  [...document.querySelectorAll('.demarche__label')].forEach((item, i) => {
+    if (i === 0) item.classList.add('demarche__label--active');
+    item.addEventListener('click', () => demarcheCarousel.select(i));
+  });
+
+
+  // Benefice section
+  [...document.querySelectorAll('.benefices__circle')].forEach((item, i) => item.addEventListener('click', () => {
+    // Remove active on all items
+    [...document.querySelectorAll('.benefices__circle')].forEach(it => it.classList.remove('benefices__circle--active'));
+    [...document.querySelectorAll('.benefices__title')].forEach(it => it.classList.remove('benefices__title--active'));
+    [...document.querySelectorAll('.benefices__descr')].forEach(it => it.classList.remove('benefices__descr--active'));
+
+    // Add class on specific items
+    item.classList.add('benefices__circle--active');
+    [...document.querySelectorAll('.benefices__title')][i].classList.add('benefices__title--active');
+    [...document.querySelectorAll('.benefices__descr')][i].classList.add('benefices__descr--active');
+  }));
+
+  // Trigger click for first element
+  [...document.querySelectorAll('.benefices__circle')][0].click();
 }
