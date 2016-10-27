d6f868678d6d8d91eaeccc93afbfbd0525cb20a3
Damien Thouvenin <damien.thouvenin@goood.pro>
2016-10-27 17:15:25 +0200

    ajout message de confirmation sur l e script de mise en prod pour viter les erreurs

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,3 +1,13 @@
+echo "!!!!!!!!! Déploiement en PROD !!!!!!!!!" 
+echo "Etes-vous sûr(e) ? tapez OUI[ENTRÉE]: " 
+read commitmsg
+if test "$commitmsg" = "OUI"
+then
+    echo "Ok, c'est parti alors."
+else
+    echo "Ah, j'ai bien fait de vérifier alors. On arrête tout.'"
+    exit -1
+fi
 set -e
 GITURL=https://github.com/gooodhub/gooodhub.github.io.git
 npm install
