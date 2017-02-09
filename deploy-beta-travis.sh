fcbfdf5e4742c402194bf1b6a10f5f81b9c294e8
nrgy <cedric.burceaux@gmail.com>
2017-02-09 19:50:18 +0100

    Delete file content version

--- deploy-beta-travis.sh
+++ deploy-beta-travis.sh
@@ -11,6 +11,7 @@ echo "#### Configuration du tag ###"
 taggen=$(uuidgen)
 echo "### tag généré : $taggen ###"
 git tag -a $taggen -m "version $taggen"
+>static/version.txt
 echo "$taggen" >> static/version.txt
 
 GITURL=https://github.com/gooodhub/goood-site-dev.git
