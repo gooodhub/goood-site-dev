a5c48a81c63a4feb6f891712810e492aece0e8d9
nrgy <cedric.burceaux@gmail.com>
2017-02-16 16:32:10 +0100

    Ajout d'événements google analytics

--- src/js/pages/formations.js
+++ src/js/pages/formations.js
@@ -5,20 +5,54 @@ export default {
   title: 'Formations',
 };
 
-ga('send', {
-  hitType: 'event',
-  eventCategory: 'AccesPage',
-  eventAction: 'acces',
-  eventLabel: 'Ouverture page' + window.location.href
-});
+function envoiGA(category, action, label){
+  ga('send', {
+    hitType: 'event',
+    eventCategory: category,
+    eventAction: action,
+    eventLabel: label
+  });
+};
 
+function envoiGABeacon(category, action, label){
+  ga('send', {
+    hitType: 'event',
+    eventCategory: category,
+    eventAction: action,
+    eventLabel: label,
+    transport: 'beacon'
+  });
+};
+
+envoiGA("AccesPage","acces", window.location.href);
+
+//formations
 $('.formation-link').on("click", function()
 {
-	ga('send', 
-	{
-	  hitType: 'event',
-	  eventCategory: 'Click',
-	  eventAction: 'Accès formation',
-	  eventLabel: 'Ouverture page'
-	});
+	envoiGABeacon('formation','acces','Click depuis /formations/ vers la formation : ' + $(this).data('title'));
+});
+
+$('#contact-formation').on("click", function(){
+	envoiGA("formation","contact", "Demande contact formation : " + $(this).data('title'));
+});
+
+$('.slide-nav-buttons__parent').on("click",function(){
+	envoiGA("formation","retour", "Retour vers les formations");
+});
+
+//agenda
+$('.cd-timeline-content').on("click", function(){
+	envoiGABeacon("agenda","click", "Accès à l'événement :" + $(this).data('title'));
+});
+
+//global
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+		envoiGA("scroll","bas de page", "utilisateur en bas de la page : " + window.location.href);
+    }
+};
+
+//main page
+$('#video-dth').on("click",function(){
+	envoiGA("home","video", "Ouverture de la pop-in vidéo");
 });
