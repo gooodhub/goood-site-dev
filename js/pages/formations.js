b9955e82e8d1740be481c0fdd0db409d789a82f8
nrgy <cedric.burceaux@gmail.com>
2017-02-16 15:51:05 +0000

    auto-commit

--- js/pages/formations.js
+++ js/pages/formations.js
@@ -5,20 +5,66 @@ export default {
   title: 'Formations',
 };
 
-ga('send', {
-  hitType: 'event',
-  eventCategory: 'AccesPage',
-  eventAction: 'acces',
-  eventLabel: 'Ouverture page' + window.location.href
+function envoiGA(category, action, label){
+  ga('send', {
+    hitType: 'event',
+    eventCategory: category,
+    eventAction: action,
+    eventLabel: label
+  });
+};
+
+function envoiGABeacon(category, action, label){
+  ga('send', {
+    hitType: 'event',
+    eventCategory: category,
+    eventAction: action,
+    eventLabel: label,
+    transport: 'beacon'
+  });
+};
+
+envoiGA("AccesPage","acces", window.location.href);
+
+//formations
+$('.formation-link').on("click", function(){
+	envoiGABeacon('formation','acces','Click depuis /formations/ vers la formation : ' + $(this).data('title'));
 });
 
-$('.formation-link').on("click", function()
-{
-	ga('send', 
-	{
-	  hitType: 'event',
-	  eventCategory: 'Click',
-	  eventAction: 'Accès formation',
-	  eventLabel: 'Ouverture page'
-	});
+$('#contact-formation').on("click", function(){
+	envoiGA("formation","contact", "Demande contact formation : " + $(this).data('title'));
+});
+
+$('.slide-nav-buttons__parent').on("click",function(){
+	envoiGA("formation","retour-formations", "Retour vers les formations");
+});
+
+//agenda
+$('.cd-timeline-content').on("click", function(){
+	envoiGABeacon("agenda","visualisation-evenement", "Accès à l'événement : " + $(this).data('title'));
+});
+
+//global
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+		envoiGA("scroll","bas de page", "utilisateur en bas de la page : " + window.location.href);
+    }
+};
+
+//main page
+$('#video-dth').on("click", function(){
+	envoiGA("home","video", "Ouverture de la pop-in vidéo");
+});
+
+//contacts
+$('#contact-sandrine').on('click', function(){
+	envoiGA("contact","demande-contact", "Demande de contact pour Paris");
+});
+
+$('#contact-gregory').on("click", function(){
+	envoiGA("contact","demande-contact", "Demande de contact pour Lyon");
+});
+
+$('#contact-romain').on("click", function(){
+	envoiGA("contact","demande-contact", "Demande de contact pour le Sud");
 });
